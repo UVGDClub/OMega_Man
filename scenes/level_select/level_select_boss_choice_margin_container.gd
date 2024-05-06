e098191468e4e384746f21c2294b07f2083df2cd
Sikowny <bsickowny@hotmail.com>
2024-05-05 20:58:14 -0700

    menu testing

--- scenes/level_select/level_select_boss_choice_margin_container.gd
+++ scenes/level_select/level_select_boss_choice_margin_container.gd
@@ -1,9 +1,25 @@
 extends MarginContainer
 
-@onready var portrait_curr = $VBoxContainer/Portrait
+@export var menuMaster: Node = null;
+@export var level: int = 0;
 
 func _process(delta: float) -> void:
-	if has_focus():
-		modulate = Color.WHITE
-	else:
-		modulate = Color.DARK_GRAY
+	if (!has_focus()):
+		modulate = Color.DARK_GRAY;
+		return;
+		
+	modulate = Color.WHITE
+	if(input_accept()):
+		signal_enter_level(level);
+	
+func input_accept():
+	return Input.is_action_pressed("act_jump")
+	
+func signal_enter_level(level):
+	#check if level valid
+	if level < 1 or level > 9: return;
+	#signal level transition
+	if (menuMaster == null): return
+	print("signal_enter_level");
+	menuMaster.enter_level(level);
+	
--- scenes/level_select/level_select_screen.gd
+++ scenes/level_select/level_select_screen.gd
@@ -1,5 +1,25 @@
 extends Control
 @onready var omega = $GridContainer/Omega
+@onready var grid_container = $GridContainer
+
 
 func _ready():
 	omega.grab_focus()
+
+func enter_level(level):
+	#TODO
+	#make screen flash
+	#do robot master intro for level
+	#this assumes it's passed good input!!
+	var next_level;
+	match(level):
+		1: next_level = load("res://scenes/level_1.tscn");
+		2: pass;
+		3: pass;
+		4: pass;
+		5: pass;
+		6: pass;
+		7: pass;
+		8: pass;
+		9: pass;
+	get_tree().change_scene_to_packed(next_level)
